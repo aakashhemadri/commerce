57ba584851cd06fc228990cbe594698b8bb0f9dd
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2021-03-31 04:43:09 +0530

    feat: Add product to cart on buy button click

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,33 +4,49 @@ import {
   Card, CardImg, CardBody,
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
+import axios from 'axios';
 
 import {
   Link,
 } from "react-router-dom";
 
-const ProductCard = (props) => {
-  return (
-    <Card>
-      <Link to={{
-        pathname: "/commerce/product",
-        state: props.item,
-      }}>
-        <CardImg top width="100%" src={props.item.imageURL} alt={props.item.name + " Image"} />
-      </Link>
-      <CardBody>
+class ProductCard extends React.Component
+{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Card>
         <Link to={{
           pathname: "/commerce/product",
-          state: props.item,
+          state: this.props.item,
         }}>
-          <CardTitle tag="h5">{props.item.name}</CardTitle>
+          <CardImg top width="100%" src={this.props.item.imageURL} alt={this.props.item.name + " Image"} />
         </Link>
-        <CardSubtitle tag="h6" className="mb-2 text-muted">{"$" + props.item.price}</CardSubtitle>
-        <Button>Add to Cart</Button>
-      </CardBody>
-    </Card >
+        <CardBody>
+          <Link to={{
+            pathname: "/commerce/product",
+            state: this.props.item,
+          }}>
+            <CardTitle tag="h5">{this.props.item.name}</CardTitle>
+          </Link>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">{"$" + this.props.item.price}</CardSubtitle>
+          <Button onClick={() => this.addToCart(this.props)} >Add to Cart</Button>
+        </CardBody>
+      </Card >
+    );
+  }
 
-  );
+  addToCart(props) {
+    axios.post('/api/cart/', {
+        products: {
+          "product": this.props.item._id,
+          "quantity": 1
+        }
+    });
+  }
 };
 
 export default ProductCard;
